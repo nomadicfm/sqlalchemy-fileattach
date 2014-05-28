0bf0f4610e7efa800906295e718be791096f2a09
Adam Charnock <adam@omniwiki.co.uk>
2014-05-28 12:00:19 +0100

    Work on porting s3 backend from django-storages

--- sqlalchemy_fileattach/exceptions.py
+++ sqlalchemy_fileattach/exceptions.py
@@ -3,3 +3,6 @@ class InvalidPathError(Exception): pass
 class NoStoreError(Exception): pass
 class InvalidFieldValue(Exception): pass
 class NoFileNameError(Exception): pass
+class ImproperlyConfigured(Exception): pass
+class SuspiciousOperation(Exception): pass
+class UnsupportedOperation(Exception): pass
