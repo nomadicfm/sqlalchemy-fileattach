faecd3d603f775e5534e963db2aebcba2299299b
Adam Charnock <adam@omniwiki.co.uk>
2014-09-06 18:15:17 +0100

    Fixing error in file field handling

--- sqlalchemy_fileattach/types.py
+++ sqlalchemy_fileattach/types.py
@@ -115,7 +115,7 @@ class FileType(TypeDecorator):
             # May be a cgi.FieldStorage instance
             f = FileProxyMixin()
             f.file = value.file
-            f.name = value.name
+            f.name = getattr(value, 'filename', value.name)
             value = f
 
         if not value:
